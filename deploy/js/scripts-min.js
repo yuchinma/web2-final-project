882b28e88861c7f74df788585e3baacd10322268
yuchinma <63366430+yuchinma@users.noreply.github.com>
2020-06-08 18:07:29 -0400

    updated js

--- deploy/js/scripts-min.js
+++ deploy/js/scripts-min.js
@@ -1,1 +1,1 @@
-var $burger=$("#burger-icon"),$nav=$("nav[data-nav]"),$navA=$("nav[data-nav] a"),isVisible=!1;function mouseClick(){console.log("click"),!1===isVisible?($nav.show(),isVisible=!0):($nav.hide(),isVisible=!1)}$burger.on("click",mouseClick),$navA.on("click",mouseClick);
+var $burger=$("#burger-icon"),$nav=$("nav[data-nav]"),$navA=$("nav[data-nav] a"),isVisible=!1;function mouseClick(){$(window).width()<=992&&(console.log("click"),!1===isVisible?($nav.show(),isVisible=!0):($nav.hide(),isVisible=!1))}$burger.on("click",mouseClick),$navA.on("click",mouseClick);
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,19 +3,21 @@ var $nav = $('nav[data-nav]');
 var $navA = $('nav[data-nav] a');
 var isVisible = false;
 
-function mouseClick(){
+function mouseClick() {
 
-    console.log("click");
-    if(isVisible === false){
-        $nav.show();
-        isVisible = true;
-    }
-    else{
-        $nav.hide();
-        isVisible = false;
+    if ($(window).width() <= 992) {
+        console.log("click");
+        if (isVisible === false) {
+            $nav.show();
+            isVisible = true;
+        } 
+        else {
+            $nav.hide();
+            isVisible = false;
+        }
     }
 }
 
-$burger.on( "click", mouseClick);
+$burger.on("click", mouseClick);
 // close the mobile menu when menu link is clicked
-$navA.on( "click", mouseClick);
+$navA.on("click", mouseClick);
